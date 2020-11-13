92f0d0916fb57c7c06cefff414d5b2d86bac6a88
Ravi <marsstrikers@gmail.com>
2020-11-13 16:23:44 +0545

    Password box designed.

--- Headers/GUICompClassA.h
+++ Headers/GUICompClassA.h
@@ -3,56 +3,59 @@
 class GUIcomponent
 {
     bool active;
+
 public:
-    GUIcomponent() {active = true;}
-    bool isActive() { return active;}
-    void setActive(bool _active) { active = _active;}
+    GUIcomponent() { active = true; }
+    bool isActive() { return active; }
+    void setActive(bool _active) { active = _active; }
 
     virtual void render() = 0;
-    virtual void mouseHandler(int button,int state,int x,int y) = 0;
-    virtual void keyboardHandler(unsigned char key,int x, int y) = 0;
+    virtual void mouseHandler(int button, int state, int x, int y) = 0;
+    virtual void keyboardHandler(unsigned char key, int x, int y) = 0;
 };
 
 class GUIPage
 {
-    std::vector<GUIcomponent*> components;
+    std::vector<GUIcomponent *> components;
+
 public:
-    void addComponent(GUIcomponent* _component)
+    void addComponent(GUIcomponent *_component)
     {
         components.push_back(_component);
     }
     void render()
     {
-        for(int i=0;i<components.size();i++)
-                components[i]->render();
+        for (int i = 0; i < components.size(); i++)
+            components[i]->render();
     }
-    void keyboardHandler(unsigned char key,int x, int y)
+    void keyboardHandler(unsigned char key, int x, int y)
     {
-        for(int i=0;i<components.size();i++)
-            if(components[i]->isActive())
+        for (int i = 0; i < components.size(); i++)
+            if (components[i]->isActive())
                 components[i]->keyboardHandler(key, x, y);
     }
-    void mouseHandler(int button,int state,int x,int y)
+    void mouseHandler(int button, int state, int x, int y)
     {
-        for(int i=0;i<components.size();i++)
-            if(components[i]->isActive())
-                components[i]->mouseHandler( button, state, x, y);
+        for (int i = 0; i < components.size(); i++)
+            if (components[i]->isActive())
+                components[i]->mouseHandler(button, state, x, y);
     }
 };
 
-class TextBox:public GUIcomponent
+class TextBox : public GUIcomponent
 {
+protected:
     bool selected;
     bool showTxt;
     std::string textField;
     Coord_Rect position;
     Color boxC;
     Color textC;
+
 public:
-    TextBox(Coord_Rect pos,Color box=Color(0,0,0),Color txt=Color(1,1,1),bool _selected =false):position(pos),boxC(box),textC(txt)
+    TextBox(Coord_Rect pos, Color box = Color(0, 0, 0), Color txt = Color(1, 1, 1), bool _selected = false) : position(pos), boxC(box), textC(txt)
     {
-        selected =_selected;
-        boxC.applyColor();
+        selected = _selected;
     }
     std::string getText()
     {
@@ -60,59 +63,58 @@ public:
     }
     void setText(std::string _text) //probably won't be used
     {
-        textField=_text;
+        textField = _text;
     }
     void showText(bool show)
     {
-        showTxt=show;
+        showTxt = show;
     }
     void render()
     {
-         glDrawP(position);
-textC.applyColor();
-         glDrawRecOutlineTextBox(position);
+        boxC.applyColor();
+        glDrawP(position);
+        textC.applyColor();
+        glDrawRecOutlineTextBox(position);
         //draw rectangular box of textbox based on position
         //if active draw one kind of box if not active draw another kind
-        if(selected || showTxt)
+        if (selected || showTxt)
         {
             textC.applyColor();
-           // printText(textField,position.x+0.1,position.y+0.2,position.x+position.width,GLUT_BITMAP_HELVETICA_12);
-           printTextInBox(textField,position,GLUT_BITMAP_HELVETICA_12);
+            // printText(textField,position.x+0.1,position.y+0.2,position.x+position.width,GLUT_BITMAP_HELVETICA_12);
+            printTextInBox(textField, position, GLUT_BITMAP_HELVETICA_12);
         }
-
     }
-     void keyboardHandler(unsigned char key,int x, int y)
+    void keyboardHandler(unsigned char key, int x, int y)
     {
         //if selected then text.push_back(key) after checking the key value
-        if(selected)
+        if (selected)
         {
-            if(key == TAB_KEY || key == ENTER_KEY)
+            if (key == TAB_KEY || key == ENTER_KEY)
             {
-               // userName = textField;
-               // std::cout<<userName;
+                // userName = textField;
+                // std::cout<<userName;
             }
-            else if(key>=36 && key<=126)
+            else if (key >= 36 && key <= 126 || key==SPACE_KEY)
             {
                 textField.push_back(key);
             }
-            else if(key=DEL_KEY && textField.size()>0)
+            else if (key = DEL_KEY && textField.size() > 0)
             {
                 textField.pop_back();
-               // userName = textField;
+                // userName = textField;
             }
         }
-
     }
-    void mouseHandler(int button,int state,int x,int y)
+    void mouseHandler(int button, int state, int x, int y)
     {
         //if button == right and state == down and liesInside(x,y) set selected = true
-        float X=toFloatX(x);
-        float Y=toFloatY(y);
-        if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && position.liesInside(X,Y))
+        float X = toFloatX(x);
+        float Y = toFloatY(y);
+        if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && position.liesInside(X, Y))
         {
-            selected=true;
+            selected = true;
         }
-        else if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+        else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
         {
             //std::cout<<userName;
             selected = false;
@@ -120,11 +122,30 @@ textC.applyColor();
     }
 };
 
-class PasswordBox:public TextBox
+class PasswordBox : public TextBox
 {
     bool showPass;
-//    PasswordBox()
+
+public:
+    PasswordBox(Coord_Rect pos, Color box = Color(0, 0, 0), Color txt = Color(1, 1, 1), bool _selected = false) : TextBox(pos, box, txt, _selected)
+    {
+
+        showPass = false;
+    }
+    /*
+    void keyboardHandler(unsigned char key,int x,int y) //NEED TO MODIFY ACCORDING TO PASSWORD STANDARDS
+    {
+    }*/
+    void render()
+    {
+        boxC.applyColor();
+        glDrawP(position);
+        textC.applyColor();
+        glDrawRecOutlineTextBox(position);
+        if (selected || showPass)
+        {
+            textC.applyColor();
+            printTextPass(TextBox::textField, TextBox::position, GLUT_BITMAP_TIMES_ROMAN_24);
+        }
+    }
 };
-
-
-
--- newGUIA.cpp
+++ newGUIA.cpp
@@ -5,79 +5,90 @@
 #include <vector>
 #include "Headers/vars.h"
 #include "Headers/GUICompClassA.h"
-Color BoxCol(0,1,0);
-Color TextCol(0,0,0);
-Coord_Rect p(-1,0,6.5,1.5);
-TextBox t1(p,BoxCol,TextCol,false);
 
-void mousePressed(int button, int state,int x, int y);
-void keyPressed(unsigned char key,int x,int y);
+Color BoxCol(0, 1, 0);
+Color passCol(0.5, 0.5, 0);
+Color TextCol(0, 0, 0);
+Coord_Rect norm(-3, 0, 6.5, 1.5);
+Coord_Rect pass(-3, -4, 6.5, 1.5);
+TextBox normBox(norm, BoxCol, TextCol, false);
+PasswordBox passB(pass, passCol, TextCol, false);
+
+void mousePressed(int button, int state, int x, int y);
+void keyPressed(unsigned char key, int x, int y);
 void callBackFun();
 void initColor();
 void ReshapeCallBack(int wid, int heig);
 int main(int argc, char **argv) //default arguments of main
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-  glutInitWindowPosition(INI_X, INI_Y);
-  glutInitWindowSize(WID, HEI);
-  glutCreateWindow("Scalling Trial");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitWindowPosition(INI_X, INI_Y);
+    glutInitWindowSize(WID, HEI);
+    glutCreateWindow("Scalling Trial");
 
-  glutDisplayFunc(callBackFun);
-  glutReshapeFunc(ReshapeCallBack);
-  glutMouseFunc(mousePressed);
-  glutKeyboardFunc(keyPressed);
-  initColor();
-  glutMainLoop();
+    glutDisplayFunc(callBackFun);
+    glutReshapeFunc(ReshapeCallBack);
+    glutMouseFunc(mousePressed);
+    glutKeyboardFunc(keyPressed);
+    initColor();
+    glutMainLoop();
 }
 void callBackFun()
 {
-  glClear(GL_COLOR_BUFFER_BIT);
-  glLoadIdentity();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
 
-
-  //printText(-2.1,3,"C++ Genie",GLUT_BITMAP_TIMES_ROMAN_24,1,1,0);
-glColor3f(0, 1,0);
-//printText(0,0,"Ravi Pandey",GLUT_BITMAP_HELVETICA_18,1,1,1);
- /* glBegin(GL_LINES);
+    //printText(-2.1,3,"C++ Genie",GLUT_BITMAP_TIMES_ROMAN_24,1,1,0);
+    glColor3f(0, 1, 0);
+    //printText(0,0,"Ravi Pandey",GLUT_BITMAP_HELVETICA_18,1,1,1);
+    /* glBegin(GL_LINES);
   glColor3f(1,1,1);
   glVertex2f(0, 0);
   glVertex2f(0, .5);
   glEnd()*/
 
-  t1.render();
-
-  glutPostRedisplay();
-  glutSwapBuffers();
+    normBox.render();
+    if (passB.isActive())
+    {
+        normBox.showText(true);
+    }
+    passB.render();
+    glutPostRedisplay();
+    glutSwapBuffers();
 }
 void ReshapeCallBack(int wid, int heig)
 {
-    glViewport(0,0,wid,heig); //full screen has been taken as view port //all drawings are displayed in the view port
-    glMatrixMode(GL_PROJECTION); //Switches to projection mode to draw stuff on the window //from GL model view matrix(default)
-    glLoadIdentity(); //resets the parameters of projection matrix
-    gluOrtho2D(-COORD_X,COORD_X,-COORD_Y,COORD_Y); //takes 4 arguments left,right,bottom,top (2D graph)
-    glMatrixMode(GL_MODELVIEW); //switches back to model-view mode,the default mode
-    //always should be in model view matrix , only go to projection matrix to set coordinates
-
-
+    glViewport(0, 0, wid, heig);                      //full screen has been taken as view port //all drawings are displayed in the view port
+    glMatrixMode(GL_PROJECTION);                      //Switches to projection mode to draw stuff on the window //from GL model view matrix(default)
+    glLoadIdentity();                                 //resets the parameters of projection matrix
+    gluOrtho2D(-COORD_X, COORD_X, -COORD_Y, COORD_Y); //takes 4 arguments left,right,bottom,top (2D graph)
+    glMatrixMode(GL_MODELVIEW);                       //switches back to model-view mode,the default mode
+                                                      //always should be in model view matrix , only go to projection matrix to set coordinates
 }
 void initColor()
 {
-  glClearColor(WC_R, WC_G, WC_B, WC_A);
+    glClearColor(WC_R, WC_G, WC_B, WC_A);
 }
 
-void mousePressed(int button, int state,int x, int y)
+void mousePressed(int button, int state, int x, int y)
 {
-    t1.mouseHandler(button,state,x,y);
-    if(state==GLUT_DOWN)
+    normBox.mouseHandler(button, state, x, y);
+    passB.mouseHandler(button, state, x, y);
+    if (state == GLUT_DOWN)
     {
-        std::cout<<"x= "<<x<<" y= "<<y<<'\n';
+        std::cout << "x= " << x << " y= " << y << '\n';
     }
 }
-void keyPressed(unsigned char key,int x,int y)
+void keyPressed(unsigned char key, int x, int y)
 {
-    t1.keyboardHandler(key,x,y);
-        userName=t1.getText();
-    std::cout<<userName<<std::endl;
+    normBox.keyboardHandler(key, x, y);
+    passB.keyboardHandler(key, x, y);
+    userName = normBox.getText();
+    passWord = passB.getText();
+    if (key == ENTER_KEY)
+    {
+        std::cout << "USER=" << userName << std::endl;
+        std::cout << "PASS=" << passB.getText() << '\n';
+    }
 }
-

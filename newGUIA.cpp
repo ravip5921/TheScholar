174ddc8773d81060912758d847d17916f5612f8b
Ravi <marsstrikers@gmail.com>
2020-11-17 11:46:25 +0545

    Text added.

--- newGUIA.cpp
+++ newGUIA.cpp
@@ -5,15 +5,19 @@
 #include <vector>
 #include "Headers/vars.h"
 #include "Headers/GUICompClassA.h"
-
 Color BoxCol(0, 1, 0);
 Color passCol(1, 1, 1);
 Color TextCol(0, 0, 0);
-Coord_Rect norm(-3, 0, 6.5, 1.5);
+Coord_Rect norm(-3, 0, 6.5, 2);
 Coord_Rect pass(-3, -4, 6.5, 1.5);
-CheckBox r(4,-4, BoxCol,false);
+Coord_Rect butD(5,-5,3,1);
+Color LogIn(1,0.17,0.3);
 TextBox normBox(norm, BoxCol, TextCol, false);
 PasswordBox passB(pass, passCol, TextCol, false);
+CheckBox r(&passB,4, -4, BoxCol, false);
+Text Login(-2,3,LogIn,"LOGIN SCREEN",GLUT_BITMAP_TIMES_ROMAN_24);
+Button loginButton(" Log In",BoxCol,LogIn,butD);
+GUIPage lp;
 
 void mousePressed(int button, int state, int x, int y);
 void keyPressed(unsigned char key, int x, int y);
@@ -27,7 +31,11 @@ int main(int argc, char **argv) //default arguments of main
     glutInitWindowPosition(INI_X, INI_Y);
     glutInitWindowSize(WID, HEI);
     glutCreateWindow("Scalling Trial");
-
+    lp.addComponent(&normBox);
+    lp.addComponent(&passB);
+    lp.addComponent(&r);
+    lp.addComponent(&Login);
+    lp.addComponent(&loginButton);
     glutDisplayFunc(callBackFun);
     glutReshapeFunc(ReshapeCallBack);
     glutMouseFunc(mousePressed);
@@ -48,13 +56,14 @@ void callBackFun()
   glVertex2f(0, 0);
   glVertex2f(0, .5);
   glEnd()*/
-    normBox.render();
+   /* normBox.render();
 
     //if (passB.isActive())
-        //normBox.showText(true);
+    //normBox.showText(true);
     passB.render();
     r.setActive(passB.isShowing());
-    r.render();
+    r.render();*/
+    lp.render();
     glutPostRedisplay();
     glutSwapBuffers();
 }
@@ -74,9 +83,10 @@ void initColor()
 
 void mousePressed(int button, int state, int x, int y)
 {
-    normBox.mouseHandler(button, state, x, y);
+  /*  normBox.mouseHandler(button, state, x, y);
     passB.mouseHandler(button, state, x, y);
-    r.mouseHandler(button,state,x,y);
+    r.mouseHandler(button, state, x, y);*/
+    lp.mouseHandler(button,state,x,y);
     if (state == GLUT_DOWN)
     {
         std::cout << "x= " << x << " y= " << y << '\n';
@@ -84,9 +94,10 @@ void mousePressed(int button, int state, int x, int y)
 }
 void keyPressed(unsigned char key, int x, int y)
 {
-    normBox.keyboardHandler(key, x, y);
+  /*  normBox.keyboardHandler(key, x, y);
     passB.keyboardHandler(key, x, y);
-    r.keyboardHandler(key,x,y);
+    r.keyboardHandler(key, x, y);*/
+    lp.keyboardHandler(key,x,y);
     if (key == ENTER_KEY)
     {
         userName = normBox.getText();
@@ -95,10 +106,11 @@ void keyPressed(unsigned char key, int x, int y)
         std::cout << "PASS=" << passB.getText() << '\n';
     }
 
- else if(key=='/')
- {
-     passB.showPass(true);
- }
- else if(key=='.')
-    passB.showPass(false);
+  /*  else if (key == '/')
+    {
+        passB.showPass(true);
+    }
+    else if (key == '.')
+
+        passB.showPass(false);*/
 }
